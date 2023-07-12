1b8249763edf969e1cae48078380d906996468ad
K.Orzel <k.orzelurban@gmail.com>
2023-07-12 17:47:33 +0200

    Update add_a_player.py

--- test_cases/add_a_player.py
+++ test_cases/add_a_player.py
@@ -1,7 +1,12 @@
 import os
 import unittest
-from selenium.webdriver.chrome.service import Service
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from pages.dashboard import Dashboard
+from pages.login_page import LoginPage
+from pages.add_player import AddPlayer
 from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
 
 
@@ -16,20 +21,27 @@ class TestAdAplayer(unittest.TestCase):
         self.driver.fullscreen_window()
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
 
-    def test_ad_a_player(self):
+    def test_add_a_player(self):
         BasePage.setUp(self)
         user_login = LoginPage(self.driver)
-        user_login.wait_for_button_will_be_clicable()
-        user_login.click_on_the_sign_in_button()
-        user_login.type_in_email('user07@getnada.com')
-        user_login.type_in_name('user')
-        user_login.type_in_surname('07')
-        user_login.type_in_age('01022012')
-        user_login.type_in_age('attacker')
-        user_login.wait_for_button_will_be_clicable()
-        user_login.click_on_the_sign_in_button()
+        user_login.title_of_page()  # check if the title of the opened page is correct
+        user_login.type_in_email('user07@getnada.com')  # enter user in the email field
+        user_login.type_in_password('Test-1234')  # enter password in the password field
+        user_login.wait_for_button_will_be_clicable()  # wait for the button to be clicable
+        user_login.click_on_the_sign_in_button()  # click on the sign in button
         dashboard_page = Dashboard(self.driver)
         dashboard_page.title_of_page()  # check if the title of the opened page is correct
+        time.sleep(10)
+        add_player = AddPlayer(self.driver)
+        add_player.type_in_email('user07@getnada.com')
+        add_player.type_in_name('user')
+        add_player.type_in_surname('aa')
+        add_player.type_in_age('01022012')
+        add_player.type_in_main_position('attacker')
+        add_player.wait_for_button_will_be_clicable()
+        add_player.click_on_the_submit_button()
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.title_of_page()
         time.sleep(10)
 
 
