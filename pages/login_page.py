9ce799aac5582a50122424316997edbbedc08b2c
K.Orzel <k.orzelurban@gmail.com>
2023-07-12 14:14:04 +0200

    login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,12 +2,14 @@ from pages.base_page import BasePage
 
 
 class LoginPage(BasePage):
+    title_of_box_xpath = "//*[@id='__next']/form/div/div[1]/h5"
     login_field_xpath = "//*[@id='login']"
     password_field_xpath = "//*[@id="password"]"
     remaind_password_hyperlink_xpath = "//*[text()="Remind password"]"
     language_listbox_xpath = "//*[@id="__next"]/form/div/div[2]/div/div"
     sign_in_button_xpath = "//*[text()='Sign in']"
     login_url = ('https://scouts-test.futbolkolektyw.pl/en/login')
+    expected_title = "Scouts panel - sign in"
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
